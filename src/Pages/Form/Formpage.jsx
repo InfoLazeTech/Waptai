a7207cd3245c0cdea84d229b5f3b3a6d59593876
aryan-infolanze <infolanzearyan@gmail.com>
2026-03-13 23:23:03 +0530

    waptai member route ui fix

--- src/Pages/Form/Formpage.jsx
+++ src/Pages/Form/Formpage.jsx
@@ -5,7 +5,7 @@ const Formpage = () => {
     <div className="w-full md:min-h-screen bg-gray-50 flex items-center justify-center md:py-25 py-25 pb-0">
 
       {/* Iframe Container */}
-      <div className="w-full max-w-4xl h-[95vh] md:h-[65vh] overflow-hidden relative md:pt-12">
+      <div className="w-full max-w-4xl h-[63vh] md:h-[65vh] moverflow-hidden relative md:pt-20">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLScw9UcDrxfhIRHVySZd5uxkGZLGEIYz_xFf5si1EyEIjiqJpw/viewform?embedded=true"
           className="w-full h-full border-0"
